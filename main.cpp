4ef01280d32a0f69d297634b4aef31f623b89c46
Krat-Roman <allier_il@mail.ru>
2015-11-05 06:19:59 +0400

    this is C++ \m/

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(0,"");
+	//-----------------------
+	
+	cout << "Hello GitHUb!" <<endl;
+
+
+	//-----------------------
+	system("pause");
+	return 0;
+}
+
